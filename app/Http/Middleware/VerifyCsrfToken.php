f2336db0c7562d2efa958560ae0cc02d88c3d01f
Sandeep <sxk80270@ucmo.edu>
2018-09-30 23:48:28 -0400

    Updated routes in web.php and csrf file

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -25,6 +25,6 @@ class VerifyCsrfToken extends Middleware
         '/courseGroup',
         '/courseSection',
         '/subject',
-        '/StudentGroupDetail'
+        '/studentGroupDetail'
     ];
 }
