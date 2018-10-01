ceabf6732a3c6a5cd237a61bb684a23d9f854f47
Sandeep <sxk80270@ucmo.edu>
2018-09-30 23:57:25 -0400

    Adding Student Rollnumber Observer

---
+++ app/Observers/StudentRollNumberObserver.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Observers;
+use App\Student;
+
+class StudentRollNumberObserver
+{
+    /**
+     * Handle the student "created" event.
+     *
+     * @param  \App\Student  $student
+     * @return void
+     */
+    public function created(Student $student)
+    {
+        $firstNameLetter = substr($student->first_name, 0, 1);
+        $lastNameLetter = substr( $student->last_name, 0, 1);
+        $dateOfBirth = substr($student->dob, 0,4);
+        $rollNumber = strtoupper($firstNameLetter) . strtolower($lastNameLetter) . '@' . $dateOfBirth;
+
+        $professor->update([
+            'roll_number' => $rollNumber,
+        ]);
+    }
+
+    /**
+     * Handle the student "updated" event.
+     *
+     * @param  \App\Student  $student
+     * @return void
+     */
+    public function updated(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Handle the student "deleted" event.
+     *
+     * @param  \App\Student  $student
+     * @return void
+     */
+    public function deleted(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Handle the student "restored" event.
+     *
+     * @param  \App\Student  $student
+     * @return void
+     */
+    public function restored(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Handle the student "force deleted" event.
+     *
+     * @param  \App\Student  $student
+     * @return void
+     */
+    public function forceDeleted(Student $student)
+    {
+        //
+    }
+}
