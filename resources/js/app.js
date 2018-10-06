ce7b18c9e2a5d88153eee767eb49f8e346fe18ed
Sandeep <sxk80270@ucmo.edu>
2018-10-06 17:03:17 -0400

    Adding Vue files

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,14 +9,25 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue') },
+    { path: '/profile',  component: require('./components/Profile.vue') }
+];
+
+const router = new VueRouter({
+    routes
+})
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
