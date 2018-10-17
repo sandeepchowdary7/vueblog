2f6ceb99b82f6796d8f8a9aea2da342739b563fe
Sandeep <sxk80270@ucmo.edu>
2018-10-16 23:50:57 -0400

    Adding composer updates

--- app/Student.php
+++ app/Student.php
@@ -6,18 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     protected $table = 'students';
-
-    protected $fillable = [
-        'first_name',
-        'middle_name',
-        'last_name',
-        'guardian_name',
-        'roll_number',
-        'gender',
-        'dob',
-        'is_active',
-        'contact_number',
-        'address',
-        'graduated_year'
-    ];
 }
