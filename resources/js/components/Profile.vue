0290075ef2ab993d293afdc25ec2f016e19cc6ea
Sandeep <sxk80270@ucmo.edu>
2018-11-01 00:53:06 -0400

    Adding HTML Templates

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -9,7 +9,7 @@
                 <h5 class="widget-user-desc">Fullstack Developer</h5>
               </div>
               <div class="widget-user-image">
-                <img class="img-circle elevation-2" src="/img/author.jpg" alt="User Avatar">
+                <img class="img-circle elevation-2" src="/img/author.png" alt="User Avatar">
               </div>
               <div class="card-footer">
                 <div class="row">
@@ -40,7 +40,205 @@
                 </div>
                 <!-- /.row -->
               </div>
-            </div>
+               <ul class="nav nav-pills">
+                  <li class="nav-item"><a class="nav-link active show" href="#activity" data-toggle="tab">Activity</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Settings</a></li>
+                </ul>
+                <div class="card-body">
+                  <div class="tab-content">
+                    <div class="tab-pane active show" id="activity">
+                      <!-- Post -->
+                      <div class="post">
+                        <div class="user-block">
+                          <img class="img-circle img-bordered-sm" src="" alt="user image">
+                          <span class="username">
+                            <a href="#">Jonathan Burke Jr.</a>
+                            <a href="#" class="float-right btn-tool"><i class="fa fa-times"></i></a>
+                          </span>
+                          <span class="description">Shared publicly - 7:30 PM today</span>
+                        </div>
+                        <!-- /.user-block -->
+                        <p>
+                          Lorem ipsum represents a long-held tradition for designers,
+                          typographers and the like. Some people hate it and argue for
+                          its demise, but others ignore the hate as they create awesome
+                          tools to help create filler text for everyone from bacon lovers
+                          to Charlie Sheen fans.
+                        </p>
+
+                        <p>
+                          <a href="#" class="link-black text-sm mr-2"><i class="fa fa-share mr-1"></i> Share</a>
+                          <a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up mr-1"></i> Like</a>
+                          <span class="float-right">
+                            <a href="#" class="link-black text-sm">
+                              <i class="fa fa-comments-o mr-1"></i> Comments (5)
+                            </a>
+                          </span>
+                        </p>
+
+                        <input class="form-control form-control-sm" type="text" placeholder="Type a comment">
+                      </div>
+                      <!-- /.post -->
+
+
+                    </div>
+                    <!-- /.tab-pane -->
+                    <div class="tab-pane" id="timeline">
+                      <!-- The timeline -->
+                      <ul class="timeline timeline-inverse">
+                        <!-- timeline time label -->
+                        <li class="time-label">
+                          <span class="bg-danger">
+                            10 Feb. 2014
+                          </span>
+                        </li>
+                        <!-- /.timeline-label -->
+                        <!-- timeline item -->
+                        <li>
+                          <i class="fa fa-envelope bg-primary"></i>
+
+                          <div class="timeline-item">
+                            <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
+
+                            <h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
+
+                            <div class="timeline-body">
+                              Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
+                              weebly ning heekya handango imeem plugg dopplr jibjab, movity
+                              jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
+                              quora plaxo ideeli hulu weebly balihoo...
+                            </div>
+                            <div class="timeline-footer">
+                              <a href="#" class="btn btn-primary btn-sm">Read more</a>
+                              <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                            </div>
+                          </div>
+                        </li>
+                        <!-- END timeline item -->
+                        <!-- timeline item -->
+                        <li>
+                          <i class="fa fa-user bg-info"></i>
+
+                          <div class="timeline-item">
+                            <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
+
+                            <h3 class="timeline-header no-border"><a href="#">Sarah Young</a> accepted your friend request
+                            </h3>
+                          </div>
+                        </li>
+                        <!-- END timeline item -->
+                        <!-- timeline item -->
+                        <li>
+                          <i class="fa fa-comments bg-warning"></i>
+
+                          <div class="timeline-item">
+                            <span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
+
+                            <h3 class="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
+
+                            <div class="timeline-body">
+                              Take me to your leader!
+                              Switzerland is small and neutral!
+                              We are more like Germany, ambitious and misunderstood!
+                            </div>
+                            <div class="timeline-footer">
+                              <a href="#" class="btn btn-warning btn-flat btn-sm">View comment</a>
+                            </div>
+                          </div>
+                        </li>
+                        <!-- END timeline item -->
+                        <!-- timeline time label -->
+                        <li class="time-label">
+                          <span class="bg-success">
+                            3 Jan. 2014
+                          </span>
+                        </li>
+                        <!-- /.timeline-label -->
+                        <!-- timeline item -->
+                        <li>
+                          <i class="fa fa-camera bg-purple"></i>
+
+                          <div class="timeline-item">
+                            <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
+
+                            <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+
+                            <div class="timeline-body">
+                              <img src="http://placehold.it/150x100" alt="..." class="margin">
+                              <img src="http://placehold.it/150x100" alt="..." class="margin">
+                              <img src="http://placehold.it/150x100" alt="..." class="margin">
+                              <img src="http://placehold.it/150x100" alt="..." class="margin">
+                            </div>
+                          </div>
+                        </li>
+                        <!-- END timeline item -->
+                        <li>
+                          <i class="fa fa-clock-o bg-gray"></i>
+                        </li>
+                      </ul>
+                    </div>
+                    <!-- /.tab-pane -->
+
+                    <div class="tab-pane" id="settings">
+                      <form class="form-horizontal">
+                        <div class="form-group">
+                          <label for="inputName" class="col-sm-2 control-label">Name</label>
+
+                          <div class="col-sm-10">
+                            <input type="email" class="form-control" id="inputName" placeholder="Name">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+
+                          <div class="col-sm-10">
+                            <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="inputName2" class="col-sm-2 control-label">Name</label>
+
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputName2" placeholder="Name">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="inputExperience" class="col-sm-2 control-label">Experience</label>
+
+                          <div class="col-sm-10">
+                            <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label for="inputSkills" class="col-sm-2 control-label">Skills</label>
+
+                          <div class="col-sm-10">
+                            <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-sm-offset-2 col-sm-10">
+                            <div class="checkbox">
+                              <label>
+                                <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                              </label>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-danger">Submit</button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                    <!-- /.tab-pane -->
+                  </div>
+                <!-- /.tab-content -->
+              </div>
+
+                  </div>
             </div>
         </div>
     </div>
