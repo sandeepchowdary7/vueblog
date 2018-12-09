b5fb31c15961d3952826d6a12041a2d6d04d26ef
Sandeep <sxk80270@ucmo.edu>
2018-12-08 19:31:43 -0500

    Adding Students Controller test-case

--- tests/Unit/StudentControllerTest.php
+++ tests/Unit/StudentControllerTest.php
@@ -1,6 +1,5 @@
 <?php
 namespace Tests\Unit;
-
 use App\Student;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -12,16 +11,18 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class StudentControllerTest extends TestCase
 {
     /**
-     * test Student Show Method.
+     * test Student Index Method.
      *
      * @return void
      */
-    public function testIncomplete()
+    public function testIndex()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-          );
+        $student = Student::all();
+     
+        $this->assertNotNull($student);
+       $this->assertCount(30, $student);
     }
+
     /**
      * test Student Show Method.
      *
@@ -33,5 +34,57 @@ class StudentControllerTest extends TestCase
         
         $this->assertNotNull($student);
         $this->assertEquals('Hyman', $student->first_name);
+    }
+    
+    /**
+     * test Student Update Method.
+     *
+     * @return void
+     */
+    public function testUpdate()
+    {
+        $student = Student::find(1);
+        $student->guardian_name = 'guardianName';
+        $student->update();
+
+        $this->assertEquals('guardianName', $student->guardian_name);
+    }
+
+    /**
+     * test Student Create Method.
+     * test Student RollNumber Generation when Create Method is Fired.
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $student = new Student;
+        $student->first_name = 'firstName';
+        $student->middle_name = 'guardianName';
+        $student->last_name = 'guardianName';
+        $student->guardian_name = 'guardianName';
+        $student->gender = 'Male';
+        $student->dob = '2015-09-06';
+        $student->contact_number = 8167455196;
+        $student->address = '7493 Legros Burg Considinefort, NH 24537';
+        $student->save();
+
+        $this->assertNotNull($student->roll_number);
+        $student = Student::all();
+        $this->assertCount(31, $student);
+    }
+
+    /**
+     * test Student Delete Method.
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $student = Student::latest()->first();
+        $student->delete();
+
+        $student = Student::all();
+        $this->assertCount(30, $student);
     }
 }
